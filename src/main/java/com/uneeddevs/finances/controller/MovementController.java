974d8673091bbdf07aa69eb6dfb5111fea7a72a7
Paulo Gabriel <pgj1085@gmail.com>
2021-10-06 21:14:25 -0300

    Document movement operations in Swagger

--- src/main/java/com/uneeddevs/finances/controller/MovementController.java
+++ src/main/java/com/uneeddevs/finances/controller/MovementController.java
@@ -1,10 +1,19 @@
 package com.uneeddevs.finances.controller;
 
+import com.uneeddevs.finances.controller.exception.StandardError;
+import com.uneeddevs.finances.controller.exception.ValidationError;
 import com.uneeddevs.finances.dto.MovementInsertDTO;
+import com.uneeddevs.finances.dto.MovementResponseDTO;
 import com.uneeddevs.finances.enums.MovementType;
 import com.uneeddevs.finances.model.Movement;
 import com.uneeddevs.finances.service.MovementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,17 +32,50 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/movements")
+@Tag(name = "Movement", description = "Movement operations")
 public class MovementController {
 
     private final MovementService movementService;
 
     @GetMapping(value = "/{uuid}")
+    @Operation(summary = "Find Movement",
+            method = "GET",
+            description = "Find movement by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found movement",
+                    content =  {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MovementResponseDTO.class))}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movement not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class))}
+            )})
     public ResponseEntity<Movement> findById(@PathVariable(value = "uuid") UUID uuid, HttpServletRequest request) {
         log.info("Receive GET to search movement with uuid {} by ip: {}", uuid,  request.getRemoteAddr());
         return ResponseEntity.ok(movementService.findById(uuid));
     }
 
     @GetMapping(value = "/search")
+    @Operation(summary = "Find Movement",
+            method = "GET",
+            description = "Find movement by period and bank account")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found movement",
+                    content =  {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = MovementResponseDTO.class)))}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movement not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class))}
+            )})
     public ResponseEntity<List<Movement>> findByPeriod(@RequestParam(value = "bankAccount") @Schema(example = "3fa85f64-5717-4562-b3fc-2c963f66afa6") UUID bankAccount,
                                                        @RequestParam(value = "start") @Schema(example = "2021-10-06T20:43:03Z") String startString,
                                                        @RequestParam(value = "end") @Schema(example = "2021-10-05T20:43:03Z") String endString) {
@@ -48,18 +90,64 @@ public class MovementController {
     }
 
     @PostMapping(value = "/input")
+    @Operation(summary = "Create new Movement",
+            method = "POST",
+            description = "Create movement input to bank account")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Found movement",
+                    content =  {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MovementResponseDTO.class))}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movement not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationError.class))}
+            )})
     public ResponseEntity<Movement> inputMovement(@Valid @RequestBody MovementInsertDTO movementInsertDTO, HttpServletRequest request) {
         log.info("Receive POST to create input movement {} by ip: {}", movementInsertDTO, request.getRemoteAddr());
         return ResponseEntity.status(HttpStatus.CREATED).body(movementService.save(movementInsertDTO.toModel(MovementType.INPUT)));
     }
 
     @PostMapping(value = "/output")
+    @Operation(summary = "Create new Movement",
+            method = "POST",
+            description = "Create movement output to bank account")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Found movement",
+                    content =  {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MovementResponseDTO.class))}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movement not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationError.class))}
+            )})
     public ResponseEntity<Movement> outputMovement(@Valid @RequestBody MovementInsertDTO movementInsertDTO, HttpServletRequest request) {
         log.info("Receive POST to create input movement {} by ip: {}", movementInsertDTO, request.getRemoteAddr());
         return ResponseEntity.status(HttpStatus.CREATED).body(movementService.save(movementInsertDTO.toModel(MovementType.OUTPUT)));
     }
 
     @DeleteMapping(value = "/{uuid}")
+    @Operation(summary = "Delete Movement by id",
+            method = "DELETE",
+            description = "Delete Movement to bank account")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Found movement"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movement not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationError.class))}
+            )})
     public ResponseEntity<Void> deleteById(@PathVariable UUID uuid, HttpServletRequest request) {
         log.info("Receive DELETE to delete movement by id {} by ip: {}", uuid, request.getRemoteAddr());
         movementService.deleteMovementById(uuid);
--- src/main/java/com/uneeddevs/finances/dto/MovementInsertDTO.java
+++ src/main/java/com/uneeddevs/finances/dto/MovementInsertDTO.java
@@ -3,17 +3,22 @@ package com.uneeddevs.finances.dto;
 import com.uneeddevs.finances.enums.MovementType;
 import com.uneeddevs.finances.model.BankAccount;
 import com.uneeddevs.finances.model.Movement;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record MovementInsertDTO(@NotNull(message = "value is mandatory")
-                                @Positive(message = "Invalid value")
-                                BigDecimal value,
-                                @NotNull(message = "bank account id is mandatory")
-                                UUID bankAccountId) {
+@Schema(name = "Movement insert")
+public record MovementInsertDTO(
+        @Schema(name = "value", example = "45.78")
+        @NotNull(message = "value is mandatory")
+        @Positive(message = "Invalid value")
+        BigDecimal value,
+        @Schema(name = "bankAccountId", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
+        @NotNull(message = "bank account id is mandatory")
+        UUID bankAccountId) {
 
     public  Movement toModel(MovementType movementType) {
         return new Movement(movementType, value, new BankAccount(bankAccountId, "insert"));
