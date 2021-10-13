7bd7cc82ebbbdc5e628927d60f9ddbe618518d7c
Paulo Gabriel <pgj1085@gmail.com>
2021-10-12 21:04:03 -0300

    CredentialDTOTest and TokenDTOTest

--- src/main/java/com/uneeddevs/finances/dto/CredentialsDTO.java
+++ src/main/java/com/uneeddevs/finances/dto/CredentialsDTO.java
@@ -1,17 +1,19 @@
 package com.uneeddevs.finances.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Builder
 @Schema(name = "Credentials")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(onConstructor = @__(@Deprecated))
 public class CredentialsDTO {
 
     @Schema(name = "email", example = "user@mail.com")
     private String email;
     @Schema(name = "password", example = "verystrongpassword")
     private String password;
-
 
 
 }
